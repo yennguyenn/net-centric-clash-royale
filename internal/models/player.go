a2e7903efb8129557631b9f1b388954dbaf8f9f7
yennguyenn <nthy0809@gmail.com>
2025-05-26 22:10:59 +0700

    Delete func NewPlayer of player.go

--- internal/models/player.go
+++ internal/models/player.go
@@ -1,5 +1,10 @@
 package models
 
+// import (
+// 	"log"
+// 	// "net-centric-clash-royale/internal/utils"
+// )
+
 type Player struct {
 	Username string  `json:"username"`
 	Password string  `json:"password"`
@@ -11,14 +16,18 @@ type Player struct {
 }
 
 // Hàm tạo Player mới
-func NewPlayer(username, password string) *Player {
-	return &Player{
-		Username: username,
-		Password: password,
-		EXP:      0,
-		Level:    1,
-		Mana:     5,
-		Towers:   []Tower{},
-		Troops:   []Troop{},
-	}
-}
+// func NewPlayer(username, password string) *Player {
+// 	// towers, err := utils.LoadPlayerTowers()
+// 	// if err != nil {
+// 	// 	log.Fatalf("❌ Failed to load towers for player %s: %v", username, err)
+// 	// }
+// 	return &Player{
+// 		Username: username,
+// 		Password: password,
+// 		// Towers:   towers,
+// 		Troops: []Troop{},
+// 		Mana:   10,
+// 		Level:  1,
+// 		EXP:    0,
+// 	}
+// }
