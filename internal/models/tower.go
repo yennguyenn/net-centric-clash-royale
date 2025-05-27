20feb27fe07f80f73957d73c8513d0f64027b043
yennguyenn <nthy0809@gmail.com>
2025-05-27 19:10:22 +0700

    Update Exp

--- internal/models/tower.go
+++ internal/models/tower.go
@@ -1,10 +1,10 @@
 package models
 
 type Tower struct {
-	Type     string  `json:"type"`
-	HP       int     `json:"hp"`
-	ATK      int     `json:"atk"`
-	DEF      int     `json:"def"`
-	CRIT     float64 `json:"crit"`
-	EXPValue int     `json:"exp"`
+	Type string  `json:"type"`
+	HP   int     `json:"hp"`
+	ATK  int     `json:"atk"`
+	DEF  int     `json:"def"`
+	CRIT float64 `json:"crit"`
+	EXP  int     `json:"exp"`
 }
