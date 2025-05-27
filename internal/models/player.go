f1bf3e686cd29bca79c958205c4bb83f65e0efcb
yennguyenn <nthy0809@gmail.com>
2025-05-27 19:09:57 +0700

    Delete func NewPlayer do replace in the init_player.go

--- internal/models/player.go
+++ internal/models/player.go
@@ -14,20 +14,3 @@ type Player struct {
 	Towers   []Tower `json:"towers"`
 	Troops   []Troop `json:"troops"`
 }
-
-// Hàm tạo Player mới
-// func NewPlayer(username, password string) *Player {
-// 	// towers, err := utils.LoadPlayerTowers()
-// 	// if err != nil {
-// 	// 	log.Fatalf("❌ Failed to load towers for player %s: %v", username, err)
-// 	// }
-// 	return &Player{
-// 		Username: username,
-// 		Password: password,
-// 		// Towers:   towers,
-// 		Troops: []Troop{},
-// 		Mana:   10,
-// 		Level:  1,
-// 		EXP:    0,
-// 	}
-// }
