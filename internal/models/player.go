33fef05433ce08a3a8a6c0b688f818d0a367ad7d
phuongthanhkkk <ITITIU21311@student.hcmiu.edu.vn>
2025-05-28 21:25:21 +0700

    update options time player.go

--- internal/models/player.go
+++ internal/models/player.go
@@ -1,16 +1,13 @@
 package models
 
-// import (
-// 	"log"
-// 	// "net-centric-clash-royale/internal/utils"
-// )
-
 type Player struct {
-	Username string  `json:"username"`
-	Password string  `json:"password"`
-	EXP      int     `json:"exp"`
-	Level    int     `json:"level"`
-	Mana     int     `json:"mana"`
-	Towers   []Tower `json:"towers"`
-	Troops   []Troop `json:"troops"`
+	Username      string    `json:"username"`
+	Password      string    `json:"password"`
+	EXP           int       `json:"exp"`
+	Level         int       `json:"level"`
+	Mana          int       `json:"mana"`
+	Towers        []Tower   `json:"towers"`
+	Troops        []Troop   `json:"troops"`
+	GameModeTimed bool      `json:"-"` // Added for game mode selection, not persisted
+	WaitChannel   chan bool `json:"-"` // Channel for signaling match found (true) or timeout (false), not persisted
 }
